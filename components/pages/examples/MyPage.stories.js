d5a76310466d4a0621dd48f3fb70ea8a0f2c952b
David Lewis <mail@david-lewis.com>
2025-11-02 08:50:04 +1100

    feat: add MyPage component with styles and stories for demonstration

---
+++ components/pages/examples/MyPage.stories.js
@@ -0,0 +1,13 @@
+import { MyPage } from './MyPage';
+
+export default {
+  title: 'Pages/Examples/MyPage',
+  component: MyPage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {
+  args: {},
+};
