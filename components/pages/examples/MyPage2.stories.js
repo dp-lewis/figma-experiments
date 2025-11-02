f632dec8b45a52b9b4ffadd1357804c14a85df95
David Lewis <mail@david-lewis.com>
2025-11-02 14:49:41 +1100

    feat: add MyPage2 component with styles and stories for demonstration

---
+++ components/pages/examples/MyPage2.stories.js
@@ -0,0 +1,17 @@
+import { MyPage2 } from './MyPage2';
+
+export default {
+  title: 'Pages/Examples/MyPage2',
+  component: MyPage2,
+  parameters: {
+    layout: 'fullscreen',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/IQTJ58xGbMYkRwRpnZe5wj/Using-the-Design-System?node-id=6-9&m=dev',
+    },
+  },
+};
+
+export const Default = {
+  args: {},
+};
